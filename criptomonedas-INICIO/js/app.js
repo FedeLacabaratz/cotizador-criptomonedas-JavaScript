d030a2fd9b26a84dd5edfc807e23b8fa7a16ea30
Fede Lacabaratz <lacarq@gmail.com>
2020-05-21 15:28:30 +0200

    add updates and finish

--- criptomonedas-INICIO/js/app.js
+++ criptomonedas-INICIO/js/app.js
@@ -1,4 +1,5 @@
-const cotizador = new API('a6b200d5b44a6811e928b6e79df61b9ff668984e5087bec47ba7c1eec4130e66');
+const YOUR_API_KEY = 'a6b200d5b44a6811e928b6e79df61b9ff668984e5087bec47ba7c1eec4130e66';
+const cotizador = new API(YOUR_API_KEY);
 const ui = new Interfaz();
 
 // Leer el formulario
